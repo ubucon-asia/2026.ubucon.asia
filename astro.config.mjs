68d6b0e63c2314b2d5418a2c2b4927f18f9561ee
Youngbin Han <ybhan@ubuntu.com>
2026-02-06 23:19:10 +0900

    set tmp baseurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/2026.ubucon.asia",
   integrations: [mdx()],
   i18n: {
     locales: ["en", "zh-tw"],
     defaultLocale: "en",
     routing: {
-      prefixDefaultLocale: false
-    }
-  }
+      prefixDefaultLocale: false,
+    },
+  },
 });
