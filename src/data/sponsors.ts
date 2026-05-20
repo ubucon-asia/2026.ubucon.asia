8a6a0f494ea9766f382232fecea5980902a5880d
Sailesh Singh <sailesh36687@gmail.com>
2026-05-20 21:55:30 +0545

    feat(data): add sponsor tier dataset
    - add Diamond, Gold, Silver, and Bronze sponsor tiers
    - include layout sizing, popup behavior, and placeholder sponsor list
    - prepares sponsor section for future sponsor entries

---
+++ src/data/sponsors.ts
@@ -0,0 +1,122 @@
+import logoPlaceHolder from "../assets/sponsors/logo_placeholder.svg";
+export const sponsors = [
+  {
+    sponsorLevelName: "Diamond",
+    sizeOnLargeScreen: 6,
+    sizeOnMediumScreen: 6,
+    sizeOnSmallScreen: 6,
+    showPopup: true,
+    list: [
+      // {
+      //   name: "Become a sponsor!",
+      //   logoUrl: logoPlaceHolder.src,
+      //   url: "/sponsors/become-a-sponsor",
+      //   description:
+      //     "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+      // },
+    ],
+  },
+  {
+    sponsorLevelName: "Gold",
+    sizeOnLargeScreen: 4,
+    sizeOnMediumScreen: 4,
+    sizeOnSmallScreen: 3,
+    showPopup: true,
+    list: [
+      // {
+      //   name: "Become a sponsor!",
+      //   logoUrl: logoPlaceHolder.src,
+      //   url: "/sponsors/become-a-sponsor",
+      //   description:
+      //     "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+      // },
+    ],
+  },
+  {
+    sponsorLevelName: "Silver",
+    sizeOnLargeScreen: 3,
+    sizeOnMediumScreen: 3,
+    sizeOnSmallScreen: 2,
+    showPopup: true,
+    list: [
+      // {
+      //   name: "Become a sponsor!",
+      //   partnerTag: "-Demo Partner",
+      //   logoUrl: logoPlaceHolder.src,
+      //   url: "/sponsors/become-a-sponsor",
+      //   description:
+      //     "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+      // },
+    ],
+  },
+  {
+    sponsorLevelName: "Bronze",
+    sizeOnLargeScreen: 2,
+    sizeOnMediumScreen: 2,
+    sizeOnSmallScreen: 1,
+    showPopup: true,
+    list: [
+      // {
+      //   name: "Become a sponsor!",
+      //   logoUrl: logoPlaceHolder.src,
+      //   url: "/sponsors/become-a-sponsor",
+      //   description:
+      //     "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+      // },
+    ],
+  },
+
+  // {
+  //   sponsorLevelName: "Supporter",
+  //   sizeOnLargeScreen: 2,
+  //   sizeOnMediumScreen: 2,
+  //   sizeOnSmallScreen: 1,
+  //   showPopup: true,
+  //   list: [
+  //     // {
+  //     //   name: "Become a sponsor!",
+  //     //   logoUrl: logoPlaceHolder.src,
+  //     //   url: "/sponsors/become-a-sponsor",
+  //     //   description:
+  //     //     "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!",
+  //     // },
+  //   ],
+  // },
+  // {
+  //   sponsorLevelName: "Startup",
+  //   sizeOnLargeScreen: 2,
+  //   sizeOnMediumScreen: 2,
+  //   sizeOnSmallScreen: 1,
+  //   showPopup: true,
+  //   list: [
+  //     // {
+  //     //   name: "Become a sponsor!",
+  //     //   logoUrl: logoPlaceHolder.src,
+  //     //   url: "/sponsors/become-a-sponsor",
+  //     //   description: "",
+  //     // },
+  //     // {
+  //     //   name: "Become a sponsor!",
+  //     //   partnerTag: "-Demo Partner",
+  //     //   logoUrl: logoPlaceHolder.src,
+  //     //   url: "/sponsors/become-a-sponsor",
+  //     //   description: "",
+  //     // },
+  //   ],
+  // },
+  // {
+  //   sponsorLevelName: "Community Partners",
+  //   sizeOnLargeScreen: 1,
+  //   sizeOnMediumScreen: 1,
+  //   sizeOnSmallScreen: 1,
+  //   showPopup: false,
+  //   list: [
+  //     // {
+  //     //   name: "Become a sponsor!",
+  //     //   logoUrl: logoPlaceHolder.src,
+  //     //   url: "",
+  //     //   description: "",
+  //     // },
+  //   ],
+  // },
+];
